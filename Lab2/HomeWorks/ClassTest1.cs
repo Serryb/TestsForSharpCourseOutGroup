d451be52f29c0eaf7f81c71aa20fd3bde5ca95ec
Sergey Ryblov <sergey.ryblov@mail.ru>
2021-11-29 10:12:36 +0300

    Lab2

--- Lab2/HomeWorks/ClassTest1.cs
+++ Lab2/HomeWorks/ClassTest1.cs
@@ -1,8 +1,16 @@
 namespace Lab2.HomeWorks
 {
-    public class ClassTest1
+    public class ClassTest1 : AClassTest, Test
     {
-        public string Print()
+        public ClassTest1 ()
+        {
+
+        }
+        public string PringFromInterface()
+        {
+            return "It is ClassTest1Interface";
+        }
+        public override string Print()
         {
             return "It is ClassTest1";
         }
